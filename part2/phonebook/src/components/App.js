cd5c53ee948061fe28bd0fd4321b1b955006ba7d
nurdtechie98 <nurdtechie98@gmail.com>
2019-09-05 13:23:27 +0530

    excercise 2.11

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect } from 'react'
 import Filter from './Filer'
 import PersonForm from './PersonForm'
 import Numbers from './Numbers'
+import axios from 'axios'
 
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterVal, setFilter ] = useState('')
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/numbers')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  useEffect(hook,[])
+  console.log('render', persons.length, 'person')
   const handleNameChange = (event)=>{
       setNewName(event.target.value);
   }
